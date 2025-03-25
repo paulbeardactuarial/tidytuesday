ad557001a6a1c98e5e310278698bb0edbee356bd
Paul Beard <paulbread88@gmail.com>
2025-03-25 14:40:39 +0000

    change font and move a little

--- 2025-03-25 report_words/2025-03-25 report_words.R
+++ 2025-03-25 report_words/2025-03-25 report_words.R
@@ -5,8 +5,15 @@ library(ggrepel)
 library(extrafont)
 library(ggtext) 
 
-# Check available fonts
+# Check/add available fonts
 fonts()
+systemfonts::register_font(
+  name = "Roboto",
+  plain = systemfonts::match_fonts("Roboto")
+)
+
+# set font
+plot_font <- "Roboto"
 
 # load in data
 # note `word_emoji_map` was constructed in `word_emoji_map_create.R` using API connection to LLM
@@ -46,22 +53,22 @@ p <-
     inherit.aes = FALSE,
     data = data.frame(),
     mapping = aes(
-      x = -Inf,
+      x = -2,
       y = 1,
       label = glue::glue(
         "'Word Cloud' showing the relative frequency of themes in Amazon Annual Reports 2005 - 2023",
         "<br>",
         "<br>",
-        "Instead of displaying words, the cloud consolidates words into themes. These were assigned as <span style='font-style:italic;color:#FF9900'>*\"the emoji that best corresponds to the word\"*</span> according to ChatGPT")
+        "Instead of displaying words, the cloud consolidates words into themes. These were assigned by ChatGPT under the prompt <span style='font-style:italic;color:#FF9900'>*\"...the emoji that best corresponds to the word\"*</span>")
     ),
     fill = NA,
     color = "#232F3E",
     box.colour = NA,
     vjust = 1,
     hjust = 0,
-    size = 4,
+    size = 4.5,
     box.padding = unit(c(0, 0, 0, 0), "pt"),
-    width = unit(1, "npc"),
+    width = unit(0.95, "npc"),
     family = plot_font
   ) +
   aes(
@@ -71,7 +78,6 @@ p <-
     size = proportion,
     data_id = emoji,
     tooltip = glue::glue(
-     # "<b>emoji: {emoji}</b>",
       "<span style='font-weight:bold;font-size:32px'>{emoji}</span>",
       "<br>",
       "<b>proportion: {scales::percent(proportion, accuracy = 0.01)}</b>",
@@ -82,14 +88,14 @@ p <-
   ) +
   geom_text_repel_interactive(
     max.overlaps = Inf,
-    seed = 2,
+    seed = 9,
     box.padding = 1,
     segment.color = NA,
-    family = "Arial",
+    family = plot_font,
     nudge_x = 0,
     nudge_y = 0,
-    xlim = c(-1, 1),
-    ylim = c(-3.5, -0.5),
+    xlim = c(-2, 2),
+    ylim = c(-5.5, -1.5),
   ) +
   theme_void() +
   scale_size(range = c(3, 20)) +
@@ -101,20 +107,20 @@ p <-
   ) +
   theme(
     legend.position = "none",
-    plot.title = element_text(size = 24, face = "bold", color = "#131A22", family = plot_font),
-    plot.subtitle = element_markdown(size = 14, face = "plain", family = plot_font, align_widths = unit(0.9, "npc"), color = "#131A22"),
+    plot.title = element_text(size = 24, face = "bold", color = "#131A22", family = plot_font, margin = margin(l = 20, r = 10)),
     plot.caption = element_text(size = 8, face = "plain", color = "#131A22", family = plot_font, margin = margin(t = 10, b = 20))
   ) +
-  coord_cartesian(xlim = c(-1, 1), ylim = c(-3.5, 1))
+  coord_cartesian(xlim = c(-2, 2), ylim = c(-5.5, 1)) 
 
 
 # make ggplot interactive 
 girafe(ggobj  = p, 
                 options = 
                   list(
-                    width_svg = 7, height_svg = 6,
+                    width_svg = 4, height_svg = 6,
                     opts_sizing(rescale = FALSE),
                     opts_tooltip(css = "background-color:#FF9900; color:black; padding: 10px; border-radius: 10px;"),
                     opts_hover(css = "opacity:0.5")
                     )
                 )
+
